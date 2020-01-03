d4d20107d57c5a30d42a5a9ec994d52f447abefc
olegsaidov <oleg_saidov@yahoo.com>
2020-01-03 19:27:00 +0500

    merged 4.3 to master branch

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 let bookList = [
- {title:"Utgan Kunlar", author: "Abdulla Qodiriy", page:350},
- {title:"Mehrobdan Chayon", author: "Abdulla Qodiriy", page:350},
- {title:"Ikki Eshik Orasi", author: "Abdulla Qodiriy", page:350},
- {title:"Daftar Chetidagi Bitiklar", author: "Abdulla Qodiriy", page:350},
- {title:"Songgi O'q", author: "Abdulla Qodiriy", page:350},
+    { title: "Utgan Kunlar", author: "Abdulla Qodiriy", page: 350 },
+    { title: "Mehrobdan Chayon", author: "Abdulla Qodiriy", page: 350 },
+    { title: "Ikki Eshik Orasi", author: "Abdulla Qodiriy", page: 350 },
+    { title: "Daftar Chetidagi Bitiklar", author: "Abdulla Qodiriy", page: 350 },
+    { title: "Songgi O'q", author: "Abdulla Qodiriy", page: 350 },
 
 ];
 
@@ -21,41 +21,34 @@ const Book = ({ title, author, page }) => {
 }
 
 
-class Library extends React.Component{
-    constructor(props){
+class Library extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            isOpen:false
+            isOpen: false
         }
     }
 
-       render(){
-           const {books} = this.props;
-           
-           return(
-          
-               <div>
-               <h2>The library is {this.state.isOpen? "open.": "closed."}</h2>
-               {books.map(
-                   (book, i)=> <Book key = {i} title = {book.title}
-                                author={book.author}
-                                page= {book.page}
-                                />
+    render() {
+        const { books } = this.props;
 
-                
+        return (
+
+            <div>
+                <h2>The library is {this.state.isOpen ? "open." : "closed."}</h2>
+                {books.map(
+                    (book, i) => <Book key={i} title={book.title}
+                        author={book.author}
+                        page={book.page}
+                    />
                 )}
-
-               </div>
-
-           )
-       
-
-
-     }
+            </div>
+        )
+    }
 }
 
 
 
 ReactDOM.render(
-    <Library books = {bookList}/>,
+    <Library books={bookList} />,
     document.getElementById('root'));
