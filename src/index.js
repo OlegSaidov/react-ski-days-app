c0aa7f81cf29abcdbd68fd90a18a3ed1eed581f5
olegsaidov <oleg_saidov@yahoo.com>
2020-01-03 14:42:00 +0500

    Create React Elements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var style = {
+    backgroundColor:"orange",
+    fontFamily: "arial",
+    fontWeight:"bold",
+    color:"white"
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const title = React.createElement(
+    "h1",
+    {
+        id:"title",
+        className:"title",
+        style:style
+    },
+
+    "Hello World"
+);
+
+ReactDOM.render(title, document.getElementById('root'));
