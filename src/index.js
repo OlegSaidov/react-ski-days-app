bb92772048874b0b6f89653ca65a8b1c189e4d4c
olegsaidov <oleg_saidov@yahoo.com>
2020-01-03 17:03:26 +0500

    Create functional component

--- src/index.js
+++ src/index.js
@@ -8,42 +8,42 @@ let skiData = {
     goal: 100
 }
 
-class SkiDayCounter extends React.Component {
-    getPercent = decimal => {
-        return decimal * 100 + "%";
-    }
+const getPercent = decimal => {
+    return decimal * 100 + "%";
+}
 
-    calcGoalProgress = (total, goal )=>{
-        return this.getPercent(total/goal);
-    }
-             
-           
-    render() {
-                const {total, powder, backcountry, goal} = this.props
-        return (
+const calcGoalProgress = (total, goal )=>{
+    return getPercent(total/goal);
+}
+     
 
-            <section>
-                <div>
-                    <p> Total Days:{total}  </p>
-                </div>
-                <div>
-                    <p> Powder Days:{powder}  </p>
-                </div>
-                <div>
-                    <p> Backcountry Days:{backcountry}  </p>
-                </div>
-                <div>
-                    <p> Goal:{goal}  </p>
-                </div>
-                <div>
-                    <p> Goal Progress:{this.calcGoalProgress(total, goal)}  </p>
-                </div>
-            </section>
+function SkiDayCounter({total, powder, backcountry,goal}){
 
-                    )
-                }
-            }
-            
+    return (
+
+        <section>
+            <div>
+                <p> Total Days:{total}  </p>
+            </div>
+            <div>
+                <p> Powder Days:{powder}  </p>
+            </div>
+            <div>
+                <p> Backcountry Days:{backcountry}  </p>
+            </div>
+            <div>
+                <p> Goal:{goal}  </p>
+            </div>
+            <div>
+                <p> Goal Progress:{calcGoalProgress(total, goal)}  </p>
+            </div>
+        </section>
+
+                )
+
+}
+
+
             
             
             ReactDOM.render(
