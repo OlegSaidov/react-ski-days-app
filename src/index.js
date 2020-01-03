adbc910e00f19f8b2bfd4b9293c03e0fb20dba0a
olegsaidov <oleg_saidov@yahoo.com>
2020-01-03 18:45:41 +0500

    Display child component, array.map

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+let bookList = [
+ {title:"Utgan Kunlar", author: "Abdulla Qodiriy", page:350},
+ {title:"Mehrobdan Chayon", author: "Abdulla Qodiriy", page:350},
+ {title:"Ikki Eshik Orasi", author: "Abdulla Qodiriy", page:350},
+ {title:"Daftar Chetidagi Bitiklar", author: "Abdulla Qodiriy", page:350},
+ {title:"Songgi O'q", author: "Abdulla Qodiriy", page:350},
+
+];
+
 const Book = ({ title, author, page }) => {
     return (
         <div>
@@ -11,17 +20,16 @@ const Book = ({ title, author, page }) => {
     )
 }
 
-const Library = () => {
+const Library = ({books}) => {
     return (
-        <div>
-            <Book title="Utgan Kunlar" author="Abudlla Qodiriy" page={350} />
-            <Book title="Mehrobdan Chayon" author="Kim Biladi" page={530} />
-            <Book title="Ikk Eshik Orasi" author="Utkir Xoshimov" page={120} />
-        </div>
+             <div>
+           {books.map(book=><Book title = {book.title} author = {book.author} page= {book.page} />
+           )}
+             </div>
     )
 }
 
 
 ReactDOM.render(
-    <Library />,
+    <Library books = {bookList}/>,
     document.getElementById('root'));
