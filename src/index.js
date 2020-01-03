0d76c5d1153fe55b36179a8fd19d004e4810d242
olegsaidov <oleg_saidov@yahoo.com>
2020-01-03 15:12:50 +0500

    Create a react component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-var style = {
-    backgroundColor:"orange",
-    fontFamily: "arial",
-    fontWeight:"bold",
-    color:"white"
+class Message extends React.Component{
+    render(){
+        return(
+            <h1>Hello Everyone!</h1>
+        )
+    }
 }
 
 
 
 ReactDOM.render(
-    <div style={style}>
-        <h1 id ="heading-element">Hello World</h1>
-        <p>Glad you're here</p>
-    </div>,
+    <Message />,
     document.getElementById('root'));
